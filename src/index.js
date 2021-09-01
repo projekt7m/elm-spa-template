c605468a1bca7219ad98f0fc96837c7174ebfc14
Matthias Wimmer <m@tthias.eu>
2021-09-01 21:13:35 +0200

    Import bootstrap in a way that variables can be overwritten

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 
 import { Elm } from './Main.elm'
 import bootstrap from 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import application from './index.scss'
 
 const app = Elm.Main.init();
